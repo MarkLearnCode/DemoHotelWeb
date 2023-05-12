8b67714239b98cf0c63ed02b50327dd0a71c493b
MarkLearnCode <111910427+MarkLearnCode@users.noreply.github.com>
2023-05-13 00:19:43 +0800

    fix orderinfo orderNo
    fix orderinfo orderNo

--- src/main/java/com/example/demo/controller/RoomorderController.java
+++ src/main/java/com/example/demo/controller/RoomorderController.java
@@ -23,38 +23,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/api")
+@RequestMapping(value = "/api")
 public class RoomorderController {
 	@Autowired
 	private RoomorderService os;
 	@Autowired
 	private RoomOrderRepository roomOrderRepository;
-	
+
 	@PostMapping("/order")
 	@CrossOrigin("*")
 	public String createOrder(@RequestBody Roomorder orderForm) {
-//		System.out.println(orderForm.getNote());
-//		System.out.println(orderForm.getWebmember().getEmail());
+		// System.out.println(orderForm.getNote());
+		// System.out.println(orderForm.getWebmember().getEmail());
 		System.out.println(orderForm.getOrderinfos());
 		String odNo = os.createNo();
 		orderForm.setOrderNo(odNo);
-//		orderForm.getOrderinfos().get(0).setRoomorder(orderForm);
-//		Orderinfo orderinfo = orderForm.getOrderinfos().get(0);
-//		System.out.println("orderinfo = "+orderinfo.getHotelName());
-//		System.out.println(orderForm.toString());
-		System.out.println("Roomorder orderForm = "+orderForm);
+		orderForm.getOrderinfos().get(0).setRoomorder(orderForm);
+		// Orderinfo orderinfo = orderForm.getOrderinfos().get(0);
+		// System.out.println("orderinfo = "+orderinfo.getHotelName());
+		// System.out.println(orderForm.toString());
+		System.out.println("Roomorder orderForm = " + orderForm);
 		os.saveOrder(orderForm);
-		return "success";		
+		return "success";
 	}
+
 	@GetMapping("/info/{email}")
 	@CrossOrigin("*")
 	public ResponseEntity<Map<String, List<Roomorder>>> findOrderInfoByEmail(@PathVariable String email) {
-        System.out.println(email);
-        List<Roomorder> details = roomOrderRepository.findByEmail(email);
-        System.out.println(details);
-        Map<String,List<Roomorder>> result=new HashMap<>();
-        result.put("data",details);
-        return ResponseEntity.ok().body(result);
-    }
+		System.out.println(email);
+		List<Roomorder> details = roomOrderRepository.findByEmail(email);
+		System.out.println(details);
+		Map<String, List<Roomorder>> result = new HashMap<>();
+		result.put("data", details);
+		return ResponseEntity.ok().body(result);
+	}
 }
-
