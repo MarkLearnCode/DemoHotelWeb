22e9aeb2ed43338a628e8f11e487b0e9e1c8022d
MarkLearnCode <111910427+MarkLearnCode@users.noreply.github.com>
2023-05-13 15:18:23 +0800

    fix query

--- src/main/java/com/example/demo/repository/OrderInfoRepository.java
+++ src/main/java/com/example/demo/repository/OrderInfoRepository.java
@@ -7,6 +7,6 @@ import com.example.demo.model.Orderinfo;
 
 public interface OrderInfoRepository extends JpaRepository<Orderinfo, Integer> {
 
-	@Query(value="SELECT COUNT(*) AS total FROM orderinfo WHERE roomType =?1 AND chkInDate <= ?2 AND chkOutDate >= ?3",nativeQuery = true)
-	int roomCount(String roomType,String chickInDate,String chickOutDate);
+	@Query(value = "SELECT COUNT(*) AS total FROM orderinfo WHERE roomType =?1 AND chkOutDate >= ?2 AND chkInDate <= ?3", nativeQuery = true)
+	int roomCount(String roomType, String chickInDate, String chickOutDate);
 }
